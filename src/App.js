3c01e37cc825b348878e617dd2b36a40850e7f7f
SangYeon Park <psyssai@gmail.com>
2019-10-12 17:20:29 +0900

    state with button test

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
-//Food Component
-function Food({name, evaluation}){
-  return (<h2>I like {name}. It is {evaluation}.</h2>)
-}
+class App extends React.Component{
+  state = {
+    count: 0
+  };
 
-//Object
-const foodILike = [
-  {
-    id : 1,
-    name: "Pizza",
-    evaluation: "Good"
-  },
-  {
-    id : 2,
-    name:"Steak",
-    evaluation:"Excellent"
+  // add
+  add = () => {
+    //this.setState({count: this.state.count + 1});
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    //this.setState({count: this.state.count - 1});
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+
+  render(){
+    return <div />;
   }
-];
-
-//props 유효성 체크
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  evaluation : PropTypes.string.isRequired
-};
-
-
-//food Render 함수
-function foodRender(food)
-{
-  return (<Food key = {food.id} name = {food.name} evaluation = {food.evaluation}/>);
-}
-
-//App Component
-function App() {
-  return (
-    <div>
-      {foodILike.map(foodRender)}
-    </div>);
 }
 
 export default App;
