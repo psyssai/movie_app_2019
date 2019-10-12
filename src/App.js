4d35fc151b75491d2590a65431d98dff9713eda2
SangYeon Park <psyssai@gmail.com>
2019-10-12 14:04:59 +0900

    Component, JSX, Props, Proptypes update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
 
+//Food Component
+function Food({name, evaluation}){
+  return (<h2>I like {name}. It is {evaluation}.</h2>)
+}
+
+//Object
+const foodILike = [
+  {
+    id : 1,
+    name: "Pizza",
+    evaluation: "Good"
+  },
+  {
+    id : 2,
+    name:"Steak",
+    evaluation:"Excellent"
+  }
+];
+
+//props 유효성 체크
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  evaluation : PropTypes.string.isRequired
+};
+
+
+//food Render 함수
+function foodRender(food)
+{
+  return (<Food key = {food.id} name = {food.name} evaluation = {food.evaluation}/>);
+}
+
+//App Component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      {foodILike.map(foodRender)}
+    </div>);
 }
 
 export default App;
